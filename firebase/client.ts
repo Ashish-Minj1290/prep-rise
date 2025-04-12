4cc5f396ae308cc2b1b210c0f4deff4f2514f6e8
Ashish <aasishtoppo1290@gmail.com>
2025-04-12 22:57:43 +0530

    bug fixed -3

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-/*const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDSIuHZo2XnsoyGWzGqrwsGSP1kJOXOg14",
     authDomain: "prep-rise.firebaseapp.com",
     projectId: "prep-rise",
@@ -16,8 +16,8 @@ import { getFirestore } from "firebase/firestore";
     messagingSenderId: "1080826867836",
     appId: "1:1080826867836:web:51b4f34d710efef3f859f9",
     measurementId: "G-ZMPLZYHVXY"
-};*/
-const firebaseConfig = {
+};
+/*const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -25,7 +25,7 @@ const firebaseConfig = {
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+};*/
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
